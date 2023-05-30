738371523655135f6700c7bf01c4eff9cd9cbe2d
Maxime Martineau <mmartineau@nexalogy.com>
2023-05-30 17:06:35 -0400

    Add build docker script for ease of use

---
+++ build-docker.sh
@@ -0,0 +1,10 @@
+VERSION=$1
+EXTRA_PARAMS=$2
+MODULE=nginx-throttle
+
+if [ -z "$VERSION" ]; then
+  docker build . --platform linux/amd64 -f Dockerfile -t ${MODULE}:latest -t nxlogy/${MODULE}:latest ${EXTRA_PARAMS}
+else
+  docker build . --platform linux/amd64 -f Dockerfile -t ${MODULE}:latest -t nxlogy/${MODULE}:latest \
+    -t ${MODULE}:${VERSION} -t nxlogy/${MODULE}:${VERSION} ${EXTRA_PARAMS}
+fi
